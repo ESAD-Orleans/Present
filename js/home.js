4666408e987137d1319177a9bda55d79777bcaba
Charlène Leroy <cleroy@esad-orleans.fr>
2015-12-03 15:13:32 +0100

    Pages averse

--- js/home.js
+++ js/home.js
@@ -1,11 +1,11 @@
-float rayon = 10; //variable décimale
-float opaciteStroke = 1;
-int mousex;
-int mousey;	
+var rayon = 10, //variable décimale
+ opaciteStroke = 1,
+mousex,
+mousey;	
 
 function setup() {
   	createCanvas(windowWidth, windowHeight);
-    background(#ffffff);
+    background(0xffffff);
     smooth();
 }
 
@@ -19,32 +19,32 @@ function windowResized() {
     
 function maintien()
 {     
-      if(!mousePressed) // si je ne clique pas
-      {
-         mousex = mouseX; // récupère en permanence les coordonnées de x et y quand je ne clique pas
-         mousey = mouseY;
-      }
-
-         if(mousePressed)
-          { 
-                if((mouseX == mousex) && (mouseY == mousey)) // 2e condition : position de la souris stable
-                {
-                  rayon+=1.8;//incremente de 1.8 le rayon
-                  opaciteStroke-=1.5; //décrémente l'opacité de 1,5
-                  stroke(0,0,0,opaciteStroke); 
-                  ellipse(mouseX,mouseY,rayon,rayon); 
-                }
-                else 
-                {
-                  opaciteStroke = 100; //réinitialise l'opacité à fond
-                  rayon = 10; //reinitialise la variable a 10
-                  background(#ffffff); //remettre le fond blanc pour effacer les cercles precedents
-                }
-          }
-          else
-          {
-                opaciteStroke = 100; //réinitialise l'opacité à fond
-                rayon = 10; //reinitialise la variable a 10
-                background(#ffffff); //remettre le fond blanc pour effacer les cercles precedents
-          }
+//      if(!mousePressed) // si je ne clique pas
+//      {
+//         mousex = mouseX; // récupère en permanence les coordonnées de x et y quand je ne clique pas
+//         mousey = mouseY;
+//      }
+//
+//         if(mousePressed)
+//          { 
+//                if((mouseX == mousex) && (mouseY == mousey)) // 2e condition : position de la souris stable
+//                {
+//                  rayon+=1.8;//incremente de 1.8 le rayon
+//                  opaciteStroke-=1.5; //décrémente l'opacité de 1,5
+//                  stroke(0,0,0,opaciteStroke); 
+//                  ellipse(mouseX,mouseY,rayon,rayon); 
+//                }
+//                else 
+//                {
+//                  opaciteStroke = 100; //réinitialise l'opacité à fond
+//                  rayon = 10; //reinitialise la variable a 10
+//                  background(0xffffff); //remettre le fond blanc pour effacer les cercles precedents
+//                }
+//          }
+//          else
+//          {
+//                opaciteStroke = 100; //réinitialise l'opacité à fond
+//                rayon = 10; //reinitialise la variable a 10
+//                background(0xffffff); //remettre le fond blanc pour effacer les cercles precedents
+//          }
 }
