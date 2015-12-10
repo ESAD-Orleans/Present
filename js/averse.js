31362144403c16f12969a1b606581311a9282316
Arthur Violy <arthur@violy.net>
2015-12-10 12:59:19 +0100

    Mise à jour du curseur

--- js/averse.js
+++ js/averse.js
@@ -1,5 +1,6 @@
 var p5canvas,
-    touchX_start, touchY_start, index = 0, pages;
+    touchX_start, touchY_start, index = 0, pages,
+    dragUIList = [];
 
 function setup() {
   	
@@ -14,13 +15,55 @@ function setup() {
 }
 
 function draw() {
-    //maintien();//appel de la fonction maintien  
+    //maintien();//appel de la fonction maintien 
+    clear();
+   dragUIList.forEach(function(dragUI){
+        dragUI.update();
+    });
+    fill('#ff0000');
+}
+
+var DragUI = function(){
+    var args = arguments,
+        self = {
+            x:args[0],
+            y:args[1],
+            starttime:(new Date()).getTime(),
+            initialize:function(){
+                dragUIList.push(this);
+                console.log(args);
+            },
+            age:function(){
+                return (new Date()).getTime() - this.starttime;
+            },
+            update:function(){
+                this.draw();
+            },
+            draw:function(){
+                var alpha = 1-Math.log(this.age()*10)/10;
+                alpha = alpha<0 ? 0 : alpha;
+                fill("rgba(0,0,0,"+alpha+")");
+                var r = Math.log(this.age()/10)*10;
+                noStroke();
+                ellipse(this.x,this.y,r,r);
+            }
+        };
+    self.initialize();
+    return self;    
 }
 
 function touchStarted() {
     clear();
     touchX_start = touchX;
     touchY_start = touchY;
+    console.log(new DragUI(touchX,touchY));
+}
+function touchMoved() {
+    var lastDragUI = dragUIList[dragUIList.length-1];
+    if(lastDragUI){
+        lastDragUI.x = touchX;
+            lastDragUI.y = touchY;
+    }
 }
 function touchEnded() {
     var mouvement = createVector(touchX-touchX_start,touchY-touchY_start),
